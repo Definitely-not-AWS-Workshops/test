61f45de75e63f04fb84c12a72afd0630f90f2deb
tulna07 <tu.lna07@gmail.com>
2024-06-30 18:00:51 +0700

    update

--- .github/scripts/check-version-exists.sh
+++ .github/scripts/check-version-exists.sh
@@ -11,8 +11,8 @@ version="$2"
 
 # Check if version exists in ECR repository
 if ! aws ecr describe-images --repository-name "${ecr_repository}" --image-ids imageTag="${version}" &> /dev/null; then
-  echo "Version $version not found in $ecr_repository ECR repository."
+  echo "Version $version not found in AWS ECR repository $ecr_repository."
   exit 1
 fi
 
-echo "Version $version found in $ecr_repository ECR repository."
+echo "Version $version found in AWS ECR repository $ecr_repository."
